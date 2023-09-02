c1573143aa92c5cb0c1d1588e15c4fbc10b640db
Adam Biśta <adamosekk2002@gmail.com>
2023-09-03 01:36:18 +0200

    styles: better styling for Post

--- client/src/components/Posts/Post.jsx
+++ client/src/components/Posts/Post.jsx
@@ -1,16 +1,31 @@
 import classes from "./Post.module.css";
-
 import ReactStars from "react-rating-stars-component";
+
 const Post = (props) => {
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
+  const maxTitleLeng = 80;
+  const maxDescriptionLeng = 250;
+  const currTitleLeng = props.title ? props.title.length : "Post Title".length;
+  const currDescriptionLeng = props.description
+    ? props.description.length
+    : "Post Description".length;
+  const cuttedDescription = props.description
+    ? props.description.slice(0, maxDescriptionLeng) +
+      (currTitleLeng > maxTitleLeng ? "..." : "")
+    : "Post Description";
+  const cuttedTitle = props.title
+    ? props.title.slice(0, maxTitleLeng) +
+      (currDescriptionLeng > maxDescriptionLeng ? "..." : "")
+    : "Post Title";
   return (
     <div className={classes.post}>
       <picture className={classes["img-wrapper"]}>
         <img className={classes.img} src={props.img}></img>
       </picture>
       <div className={classes["post-details"]}>
-        <h2 className={classes["post-title"]}>
-          {props.title ? props.title : "Post Title"}
-        </h2>
+        <h2 className={classes["post-title"]}>{cuttedTitle}</h2>
         <p className={classes["person-info"]}>
           <span className={classes["person-name"]}>
             {props.personName ? props.personName : "Person Name"}
@@ -19,10 +34,24 @@ const Post = (props) => {
             {props.date ? props.date : "Post Date"}
           </time>
         </p>
-        <p className={classes["post-description"]}>
-          {props.description ? props.description : "Post Description"}
-        </p>
-        <button className={classes["read-more"]}>Read more</button>
+        <p className={classes["post-description"]}>{cuttedDescription}</p>
+        <div className={classes["row-container"]}>
+          <button className={classes["read-more"]}>Read more</button>
+          <div className={classes["react-stars"]}>
+            <ReactStars
+              count={5}
+              onChange={ratingChanged}
+              size={32}
+              value={props.rating ? props.rating : 0}
+              edit={false}
+              isHalf={true}
+              emptyIcon={<i className="far fa-star"></i>}
+              halfIcon={<i className="fa fa-star-half-alt"></i>}
+              fullIcon={<i className="fa fa-star"></i>}
+              activeColor="#ffd700"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
