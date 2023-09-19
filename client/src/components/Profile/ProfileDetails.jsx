e4d27ccfa4caeb929df4dd87b906ff2abe11c68a
Adam Biśta <adamosekk2002@gmail.com>
2023-09-19 16:40:13 +0200

    feat(ProfileDetails): Add feedback mechanism when a user tries to update their profile

--- client/src/components/Profile/ProfileDetails.jsx
+++ client/src/components/Profile/ProfileDetails.jsx
@@ -1,48 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./ProfileDetails.module.css";
 import { formatShortMonthDate } from "../../utils/date-conventer";
 import defaultProfileImage from "../../assets/images/profilePicture.png"; // Przyjmując, że masz obraz domyślny w tym miejscu
 import { FiEdit } from "react-icons/fi";
-import AutoExpandTextArea from "../../ui/AutoExpandTextArea.jsx"; 
-import {useMutation} from '@tanstack/react-query'
-import {updateUser,queryClient} from '../../utils/http'
-
+import AutoExpandTextArea from "../../ui/AutoExpandTextArea.jsx";
+import { useMutation } from "@tanstack/react-query";
+import { updateUser, queryClient } from "../../utils/http";
+import LoadingOverlay from "../../ui/LoadingOverlay/LoadingOverlay.jsx";
 const ProfilePicture = ({ imageSrc, description }) => (
   <div className={styles.profileImageContainer}>
     <img
       src={imageSrc || defaultProfileImage}
       alt={description || "User profile"}
     />
-    
   </div>
 );
 
 const ProfileDetails = (props) => {
-  const { created, email, name, surname, profilePicture, description } = props.user;
-  const {mutate,isPending,isError,error} = useMutation({
-    mutationFn:updateUser,
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState("");
+  const [modalType, setModalType] = useState("");
+  const { created, email, name, surname, profilePicture, description } =
+    props.user;
+  const { mutate, isPending } = useMutation({
+    mutationFn: updateUser,
     onMutate: async (data) => {
-      const fields = data.updatedFields
+      const fields = data.updatedFields;
       await queryClient.cancelQueries(["profile"]);
-      const previousProfile = queryClient.getQueryData([
-        "profile"
-      ]);
+      const previousProfile = queryClient.getQueryData(["profile"]);
 
       const newUserObj = {
         ...previousProfile,
-        ...fields
-      }
+        ...fields,
+      };
       queryClient.setQueryData(["profile"], newUserObj);
       return { previousProfile };
     },
     onError: (err, data, context) => {
-      console.log(err)
-      queryClient.setQueryData(["profile", articleId], context.previousProfile);
+      queryClient.setQueryData(["profile"], context.previousProfile);
+      setModalMessage("Operation failed!"); // Przykładowa wiadomość
+      setModalType("error");
+      setShowModal(true);
+    },
+    onSuccess: (data) => {
+      console.log("TAK!");
+      setModalMessage("Your data has been updated!"); // Przykładowa wiadomość
+      setModalType("success");
+      setShowModal(true);
     },
     onSettled: () => {
       queryClient.invalidateQueries(["profile"]);
     },
-  })
+  });
   const [isEditing, setIsEditing] = useState({
     name: false,
     surname: false,
@@ -70,16 +79,49 @@ const ProfileDetails = (props) => {
   };
 
   const handleFieldSave = (field) => {
-    mutate({updatedFields:editedFields})
-    handleEditToggle(field); 
+    if (editedFields[field] !== props.user[field]) {
+      mutate(
+        { updatedFields: editedFields },
+        {
+          onError: () => {
+            setEditedFields((prev) => ({
+              ...prev,
+              [field]: props.user[field],
+            }));
+          },
+        }
+      );
+    }
+    handleEditToggle(field);
   };
-  
+
+  useEffect(() => {
+    if (showModal) {
+      const timeoutId = setTimeout(() => {
+        setShowModal(false);
+      }, 3000);
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [showModal]);
+
   return (
     <div className={styles.profileContainer}>
+      <div
+        className={`${styles.modalMessage} ${showModal ? styles.active : ""} ${
+          modalType === "error" ? styles.error : styles.success
+        }`}
+      >
+        
+        {modalMessage}
+      </div>
+      {isPending && <LoadingOverlay />}
       <div className={styles.containerInfo}>
-        <ProfilePicture imageSrc={props.profilePicture} description={"User profile"} />
+        <ProfilePicture
+          imageSrc={props.profilePicture}
+          description={"User profile"}
+        />
         <div className={styles.profileDetails}>
-  
           <div className={styles.editContainer}>
             {isEditing.name || isEditing.surname ? (
               <>
@@ -89,8 +131,8 @@ const ProfileDetails = (props) => {
                   value={editedFields.name}
                   onChange={(e) => handleFieldChange("name", e.target.value)}
                   onBlur={() => {
-                    handleFieldSave("name")
-                    handleFieldSave("surname")
+                    handleFieldSave("name");
+                    handleFieldSave("surname");
                   }}
                 />
                 <input
@@ -99,57 +141,74 @@ const ProfileDetails = (props) => {
                   value={editedFields.surname}
                   onChange={(e) => handleFieldChange("surname", e.target.value)}
                   onBlur={() => {
-                    handleFieldSave("surname")
-                    handleFieldSave("name")
+                    handleFieldSave("surname");
+                    handleFieldSave("name");
                   }}
                 />
-                <button className={styles.save} type="button" onClick={()=>{
-                  handleFieldSave("name")
-                  handleFieldSave("surname")
-                }}>Save</button>
+                <button
+                  className={styles.save}
+                  type="button"
+                  onClick={() => {
+                    handleFieldSave("name");
+                    handleFieldSave("surname");
+                  }}
+                >
+                  Save
+                </button>
               </>
             ) : (
               <>
-                <p className={styles.fullName}>
-                  {editedFields.name}
-                </p>
-                <p className={styles.fullName}>
-                  {editedFields.surname}
-                </p>
-                <FiEdit onClick={() =>{ 
-                  handleEditToggle("surname")
-                  handleEditToggle("name")
-                  }} style={{ cursor: "pointer", marginLeft: "5px" }} />
+                <p className={styles.fullName}>{editedFields.name}</p>
+                <p className={styles.fullName}>{editedFields.surname}</p>
+                <FiEdit
+                  onClick={() => {
+                    handleEditToggle("surname");
+                    handleEditToggle("name");
+                  }}
+                  style={{ cursor: "pointer", marginLeft: "5px" }}
+                />
               </>
             )}
           </div>
-  
+
           {isEditing.description ? (
             <>
-            <AutoExpandTextArea
-                value={editedFields.description || ''}
-                onChange={(e) => handleFieldChange("description", e.target.value)}
+              <AutoExpandTextArea
+                value={editedFields.description || ""}
+                onChange={(e) =>
+                  handleFieldChange("description", e.target.value)
+                }
                 maxLength={300} // Przykładowa maksymalna długość, dostosuj według potrzeb
                 className={styles.textArea} // Jeśli masz style dla tego komponentu
               />
-            <button className={styles.save} type="button" onClick={()=>{
-                  handleFieldSave("description")
-                }}>Save</button>
+              <button
+                className={styles.save}
+                type="button"
+                onClick={() => {
+                  handleFieldSave("description");
+                }}
+              >
+                Save
+              </button>
             </>
           ) : (
             <p className={styles.description}>
-              {editedFields.description || 'Describe yourself'}
-              <FiEdit onClick={() => handleEditToggle("description")} style={{ cursor: "pointer", marginLeft: "5px" }} />
+              {editedFields.description || "Describe yourself"}
+              <FiEdit
+                onClick={() => handleEditToggle("description")}
+                style={{ cursor: "pointer", marginLeft: "5px" }}
+              />
             </p>
           )}
-  
+
           <p className={styles.email}>{email}</p>
-          <p className={styles.creationDate}>created {formatShortMonthDate(created)}</p>
+          <p className={styles.creationDate}>
+            created {formatShortMonthDate(created)}
+          </p>
         </div>
       </div>
     </div>
   );
-  
 };
 
 export default ProfileDetails;
